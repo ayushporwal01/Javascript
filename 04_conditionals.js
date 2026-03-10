ef58d2c81b3c8708ae52f4b2bda6bbc5aa814660
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 22:06:18 +0530

    Initial commit

--- 04_conditionals.js
+++ 04_conditionals.js
@@ -6,3 +6,33 @@ if(age >= 18) {
 } else {
     console.log("You cannot watch adult content")
 }
+
+//Switch
+let day = 1
+
+switch (day) {
+    case 1:
+        console.log("Sunday")
+        break;
+    case 2:
+        console.log("Monday")
+        break;
+    case 3:
+        console.log("Tuesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 4:
+        console.log("Wednesday")
+        break;
+
+    default:
+        break;
+}
