79241de1595dc141f2cc1e60baee7a0648973585
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 22:07:06 +0530

    Initial commit

--- 04_conditionals.js
+++ 04_conditionals.js
@@ -21,18 +21,18 @@ switch (day) {
         console.log("Tuesday")
         break;
     case 4:
-        console.log("Thursday")
-        break;
-    case 5:
-        console.log("Friday")
-        break;
-    case 6:
-        console.log("Saturday")
-        break;
-    case 4:
         console.log("Wednesday")
         break;
-
+    case 5:
+        console.log("Thursday")
+        break;
+    case 6:
+        console.log("Friday")
+        break;
+    case 7:
+        console.log("Saturday")
+        break;
     default:
+         console.log("Invalid Day!")
         break;
 }
