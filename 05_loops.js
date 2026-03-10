353a882cb1448a38641c24a2c7b23852ec44ebbb
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 23:00:33 +0530

    Initial commit

--- 05_loops.js
+++ 05_loops.js
@@ -25,18 +25,18 @@ do {
 while(i <= 5)
 
 //For Of - Used to loop through arrays or iterable values.
-// let arr = [10, 20, 30]
+let arr = [10, 20, 30]
 
-// for(let value of arr){
-//   console.log(value)
-// }
+for(let value of arr){
+  console.log(value)
+}
 
 // //For In - Used to loop through object properties.
-// let person = {
-//   name: "Rahul",
-//   age: 20
-// };
+let person = {
+  name: "Rahul",
+  age: 20
+};
 
-// for(let key in person){
-//   console.log(key, person[key])
-// }
+for(let key in person){
+  console.log(key, person[key])
+}
