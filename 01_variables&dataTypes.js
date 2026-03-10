56b0d8c0e0ed885fb874d5c6cc8d39020d71c8a6
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 20:42:19 +0530

    Initial commit

--- 01_variables&dataTypes.js
+++ 01_variables&dataTypes.js
@@ -3,9 +3,27 @@ let a = 10
 console.log(a)
 
 //Data Types
+
+//Primitive
 let name = "ayush"
 let age = 21
 let cgpa = 7.65
 let isStudent = true
 let x = undefined;
 let data = null
+let bigNum = 12345678901234567890n;
+
+//Non-primitive
+
+function greet() {
+  console.log("Hello");
+}
+
+//arrays
+let numbers = [1, 2, 3, 4];
+
+//object
+let person = {
+  name: "Rahul",
+  age: 21
+};
