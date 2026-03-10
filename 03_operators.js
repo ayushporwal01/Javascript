cdcf9f2b41715f8c0de9a2e5137a4461de447240
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 21:16:36 +0530

    Initial commit

--- 03_operators.js
+++ 03_operators.js
@@ -1,3 +1,12 @@
 let a = 4
 let b = 5
 
+//Arithmetic
+console.log("a + b: ", a+b)
+console.log("a - b: ", a-b)
+console.log("a * b: ", a*b)
+console.log("a / b: ", a/b)
+console.log("a % b: ", a%b)
+console.log("a ** b: ", a**b)
+console.log("a++: ", a++)
+console.log("b--: ", b--)
