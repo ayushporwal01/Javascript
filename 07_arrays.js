1502d1af7ea87237b6b3840bca4abdeb42fe75f2
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 23:15:08 +0530

    Initial commit

--- 07_arrays.js
+++ 07_arrays.js
@@ -1,3 +1,19 @@
 let arr = [10, "Hello", true]
 console.log(arr)
 console.log(arr[1])
+
+arr[2] = false
+console.log(arr)
+console.log(arr.length)
+
+arr.push(30)
+console.log(arr)
+
+arr.unshift(5)
+console.log(arr)   //
+
+arr.pop()
+console.log(arr)   //removes last
+
+arr.shift()
+console.log(arr)  //removes first 
