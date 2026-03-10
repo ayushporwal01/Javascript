2782284e307ebd78801ac45ebc7c6bdcba88c784
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 21:36:12 +0530

    Initial commit

--- 03_operators.js
+++ 03_operators.js
@@ -34,7 +34,7 @@ console.log("x %= 5: ")
 
 //Comparison
 console.log("a == b: ", a==b)
-console.log("a += 5: ", a+=5)
+console.log("a += 5: ", a===5)
 console.log("a -= 5: ", a-=5)
 console.log("a *= 5: ", a*=5)
 console.log("a /= 5: ", a/=b)
