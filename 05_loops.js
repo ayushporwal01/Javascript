15eb9924190fc24fd138875ca1af234e5e401238
ayushporwal01 <porwalayush953@gmail.com>
2026-03-10 22:30:04 +0530

    Initial commit

--- 05_loops.js
+++ 05_loops.js
@@ -18,3 +18,20 @@ do {
    i++
 }
 while(i <= 5)
+
+//For Of - Used to loop through arrays or iterable values.
+let arr = [10, 20, 30];
+
+for(let value of arr){
+  console.log(value);
+}
+
+//For In
+let person = {
+  name: "Rahul",
+  age: 20
+};
+
+for(let key in person){
+  console.log(key, person[key]);
+}
