adfedc6305c889711913c23fadce126dbfa713d7
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 19:14:32 +0530

    Initial commit

--- 17_call-apply-bind.js
+++ 17_call-apply-bind.js
@@ -12,3 +12,13 @@ greet.call(person, 21);
 
 
 //apply - Almost the same as call(), but it passes arguments as an array.
+
+const person2 = {
+  name: "Rahul"
+};
+
+function greet(age, city) {
+  console.log(this.name + " is " + age + " from " + city);
+}
+
+greet.apply(person2, [21, "Delhi"]);
