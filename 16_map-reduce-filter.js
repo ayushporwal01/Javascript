44f9a08b3fb694b082e8437c4901e1d1deb8e426
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 18:58:25 +0530

    Initial commit

---
+++ 16_map-reduce-filter.js
@@ -0,0 +1,9 @@
+//map - Transforms every element of an array.
+
+const nums = [1, 2, 3, 4, 5]
+
+const doubled = nums.map(num => num * 2)
+console.log(doubled)
+
+//filter - Select only those elements that satisfies a given condition. 
+
