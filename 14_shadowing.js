9278150ce2569e31403fd80daf0f50cfd1c14d04
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 18:31:00 +0530

    Initial commit

--- 14_shadowing.js
+++ 14_shadowing.js
@@ -1,3 +1,7 @@
+/*Shadowing - when an inner scope variable has the same name as an outer scope variable. 
+The inner variable temporarily hides the outer variable inside its scope.*/
+
+
 let a = 10
 
 function x() {
