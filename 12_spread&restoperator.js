6ff3ad16f2c79beabbeb978ff46d3504e52b968a
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 17:32:41 +0530

    Initial commit

--- 12_spread&restoperator.js
+++ 12_spread&restoperator.js
@@ -1,6 +1,11 @@
 //Spread Operator(...) - used to expand an array or obj into individual elements.
 
+//Arrays
 const nums = [1, 7, 69]
 const nums2 = [...nums, 4, 88]
 
 console.log(nums2)
+
+//Objects
+const person = {name: "Ayush", age: 21}
+const person2 = {...person, city: "Miami"}
