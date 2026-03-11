0b825f389aafb677686f60db8a42bead56fa864e
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 16:57:46 +0530

    Initial commit

--- 08_objects.js
+++ 08_objects.js
@@ -6,16 +6,16 @@ let person = {
 
 console.log(person)
 
-//Accessing
+//Accessing Properties
 console.log(person.name)
 console.log(person.age)
 console.log(person["city"])
 
-//Adding & Updating
+//Adding & Updating Properties
 person.country = "Japan"
 person.city = "Tokyo"
 console.log(person)
 
-//Deleting
+//Deleting Properties
 delete person.city
 console.log(person)
