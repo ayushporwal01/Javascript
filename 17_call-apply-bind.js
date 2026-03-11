d35c07456226c763b8dbc61594622a63c3d3d053
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 19:13:30 +0530

    Initial commit

--- 17_call-apply-bind.js
+++ 17_call-apply-bind.js
@@ -1,2 +1,11 @@
 //call - invokes the function immediately and lets you set the value of this. 
 
+const person = {
+  name: "Rahul"
+};
+
+function greet(age) {
+  console.log("Hello " + this.name + ", age " + age);
+}
+
+greet.call(person, 21);
