256c7427feef7279788dbdb5e50e68aff8360c31
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 17:41:46 +0530

    Initial commit

--- 13_default-parameter.js
+++ 13_default-parameter.js
@@ -1,1 +1,8 @@
 //Default Parameter - A value that a function uses if no argument is provided during the function call.
+
+function greet(name = "Bhai") {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("Ayush"); // Hello, Ayush!
+greet();        // Hello, Guest!
