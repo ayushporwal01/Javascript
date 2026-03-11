24f8eb58dd12d4e07ede5e62507abc121c794546
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 19:23:49 +0530

    Initial commit

--- 17_call-apply-bind.js
+++ 17_call-apply-bind.js
@@ -1,4 +1,6 @@
 //call - Invokes the function immediately and lets you set the value of this. 
+//Runs Immediately
+//Arguments are passed separately
 
 const person = {
   name: "Rahul"
@@ -12,6 +14,7 @@ greet.call(person, 21);
 
 
 //apply - Almost the same as call(), but it passes arguments as an array.
+//Arguments are passed as an array
 
 const person2 = {
   name: "Rahul"
@@ -25,7 +28,11 @@ greet2.apply(person2, [21, "Delhi"]);
 
 
 //bind - It does NOT run the function immediately.
-//It returns a new function with this fixed.
+//It returns a new function with - this fixed.
+//Doesn't run immediately
+//Returns a new function
+//You call it later
+
 
 const person3 = {
   name: "Rahul"
