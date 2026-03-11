f4380c4b60fabac326e3ba014d2ee99b75e0a805
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 17:29:17 +0530

    Initial commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,25 +1,3 @@
-let arr = [10, "Hello", true]
-// console.log(arr)
 
-// //Accessing
-// console.log(arr[1])
-
-// //Updating
-// arr[2] = false
-// console.log(arr)
-
-// //Length
-// console.log(arr.length)
-
-// //Methods
-// arr.push(30)        //add at end
-// console.log(arr)
-
-// arr.unshift(5)      //add at beginning
-// console.log(arr)  
-
-// arr.pop()
-// console.log(arr)    //removes last
-
-// arr.shift()
-// console.log(arr)    //removes first 
+console.log(name)
+console.log(age)
