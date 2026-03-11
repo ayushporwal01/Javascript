3d1a25a7dd621eb044a27de5edbe68e837a30e20
ayushporwal01 <porwalayush953@gmail.com>
2026-03-11 19:00:28 +0530

    Initial commit

--- 16_map-reduce-filter.js
+++ 16_map-reduce-filter.js
@@ -5,5 +5,7 @@ const nums = [1, 2, 3, 4, 5]
 const doubled = nums.map(num => num * 2)
 console.log(doubled)
 
-//filter - Select only those elements that satisfies a given condition. 
+//filter - Select only those elements that satisfies a given condition.
 
+const oddNums = nums.filter((e) => num * 2 == 0)
+console.log(oddNums)
