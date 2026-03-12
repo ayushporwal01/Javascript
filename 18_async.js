25f24408900b4bb7bead9fe7ad72c1d18b4a2e83
ayushporwal01 <porwalayush953@gmail.com>
2026-03-12 22:32:21 +0530

    Initial commit

--- 18_async.js
+++ 18_async.js
@@ -8,4 +8,18 @@ console.log("Start");
 
 
 //Promises - An object that represents the eventual completion or failure of an async operation.
+const promise = new Promise((resolve, reject) => {
+  let success = false;
 
+  setTimeout(() => {
+    if (success) {
+      resolve("Data received");
+    } else {
+      reject("Error fetching data");
+    }
+  }, 2000);
+});
+
+promise
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
