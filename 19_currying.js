799612c7aeedf0247afc8ea29c2422a3a6104ebd
ayushporwal01 <porwalayush953@gmail.com>
2026-03-13 10:49:16 +0530

    Initial commit

--- 19_currying.js
+++ 19_currying.js
@@ -7,6 +7,7 @@ function add(a) {
 console.log(add(2)(3))
 
 //Arrow Function
-const add = a => b => a + b;
-
-console.log(add(2)(3)); 
+const add2 = a => b => c => a + b + c;
+const addition = add2(2);
+console.log(addition(2)(3)); 
+console.log(addition(5)(6)); 
