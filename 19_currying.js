54ff9c0560cf87b9bced22ce3e5b7b3e38b278e3
ayushporwal01 <porwalayush953@gmail.com>
2026-03-13 10:50:52 +0530

    Initial commit

--- 19_currying.js
+++ 19_currying.js
@@ -7,7 +7,13 @@ function add(a) {
 console.log(add(2)(3))
 
 //Arrow Function
-const add2 = a => b => c => a + b + c;
-const addition = add2(2);
+const add2 = a => b => a + b;
+console.log(add2(2)(3)); 
+console.log(add2(5)(6)); 
+
+
+//a = 2 is fixed now
+const add3 = a => b => c => a + b + c;
+const addition = add3(2);
 console.log(addition(2)(3)); 
 console.log(addition(5)(6)); 
