686f11354f3b5e3a4d803ab91c0c7965593b13e2
ayushporwal01 <porwalayush953@gmail.com>
2026-03-20 11:59:54 +0530

    Initial commit

--- 21_exception_handling.js
+++ 21_exception_handling.js
@@ -11,3 +11,15 @@ try {
 }
 
 //2. Throwing Manual Exceptions
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero!");
+  }
+  return a / b;
+}
+
+try {
+  console.log(divide(10, 0));
+} catch (err) {
+  console.error("Error:", err.message);
+}
