a40b8ba1d39ceb51b1044f5379668e06b498e5eb
ayushporwal01 <porwalayush953@gmail.com>
2026-03-20 11:58:38 +0530

    Initial commit

--- 21_exception_handling.js
+++ 21_exception_handling.js
@@ -1,12 +1,9 @@
 //Exception Handling - handling errors
 
 //1. try-catch
-
 try {
-  // Code that might throw an error
   let result = riskyOperation();
   console.log(result);
 } catch (error) {
-  // Handle the error
   console.error("An error occurred:", error.message);
 }
