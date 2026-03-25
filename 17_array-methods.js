51aab5ea3a7faa7ee167fb0d4437a4058ede096a
ayushporwal01 <porwalayush953@gmail.com>
2026-03-25 16:44:59 +0530

    Initial commit

--- 17_array-methods.js
+++ 17_array-methods.js
@@ -1,6 +1,7 @@
 //map - Transforms every element of an array.
 
 const nums = [1, 2, 3, 4, 5]
+const string = "leon"
 
 const doubled = nums.map(num => num * 2)
 console.log(doubled)
@@ -26,7 +27,6 @@ const index = nums.findIndex(x => x == 2)
 console.log(index)
 
 //slice - extract a section of string and return a new string with extracted elements.
-
-const index = nums.findIndex(x => x == 2) 
+const extracted = nums.slice() 
 console.log(index)
 
