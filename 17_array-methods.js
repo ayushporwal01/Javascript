6f277ed6b8ff8295e5b74fe50390e5872f097a30
ayushporwal01 <porwalayush953@gmail.com>
2026-03-25 17:01:30 +0530

    Initial commit

--- 17_array-methods.js
+++ 17_array-methods.js
@@ -30,7 +30,7 @@ console.log(index)
 const extracted = name.slice(0, -1) 
 console.log(extracted)
 
-//splice - extract a section of string and return a new string with extracted elements.
+//splice - Returns an array with extracted elements within the specified range, modifies the original array.
 const splitted = name.split(0, -1) 
 console.log(splitted)
 
