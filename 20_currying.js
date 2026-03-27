ee8af5fc49038adfa9ab4d916570625eb924ae2f
ayushporwal01 <porwalayush953@gmail.com>
2026-03-27 09:29:27 +0530

    Initial commit

--- 20_currying.js
+++ 20_currying.js
@@ -1,5 +1,5 @@
-// /*Currying is transforming a function that takes multiple arguments into a 
-// sequence of functions, each taking one argument at a time.*/
+/*Currying is transforming a function that takes multiple arguments into a 
+sequence of functions, each taking one argument at a time.*/
 
 //Using bind
 let multiply = function(x, y) {
